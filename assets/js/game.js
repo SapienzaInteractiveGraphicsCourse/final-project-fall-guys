a22b74118c4a5318cb7890c24c5f98747fd402ed
Andrea Panceri <pancer.apps@gmail.com>
2023-07-03 17:44:52 +0200

    SkyBox and Water

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,68 +1,127 @@
 const Assets = {
     textures: {
         carpet: {
-            Url: "assets/images/carpet.png"
+            Url: "assets/images/carpet.jpg"
+        },
+        sky: {
+            Url: "assets/images/skybox/skybox.env"
+        },
+        water: {
+            Url: "assets/images/waterbump.png"
+        },
+    },
+    models: {
+        player: {
+            Url: "assets/static/models/player/"
         }
     }
 };
 
-//MOVEMENT
-var keyStatus = { 87: false, 65: false, 83: false, 68: false };
+BABYLON.DefaultLoadingScreen.prototype.displayLoadingUI = function () {
+    return;
+};
+
+BABYLON.DefaultLoadingScreen.prototype.hideLoadingUI = function () {
+    document.getElementById("spinner").style.display = "none";
+    console.log("Scene is now loaded");
+}
+
+var startRenderLoop = function (engine, canvas) {
+    engine.runRenderLoop(function () {
+        if (sceneToRender && sceneToRender.activeCamera) {
+            move_player();
+            sceneToRender.render();
+        }
+    });
+}
+
+var createDefaultEngine = function () {
+    return new BABYLON.Engine(canvas, true, {
+        preserveDrawingBuffer: true,
+        stencil: true,
+        disableWebGL2Support: false
+    });
+};
+
+//CONFIGURATION
+const canvas = document.getElementById("renderCanvas");
+var BabylonEngine = null;
+var sceneToRender = null;
+
+var cubes = [];
+const keyStatus = { 87: false, 65: false, 83: false, 68: false };
 var movementAmount = 0.1; // Adjust the movement speed as needed
-
-const canvas = document.getElementById("renderCanvas"); // Get the canvas element
-const BabylonEngine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D BabylonEngine
-
 
 
 const createScene = function () {
     configure_movement_listeners();
+    BabylonEngine.displayLoadingUI();
     // Creates a basic Babylon Scene object
     const scene = new BABYLON.Scene(BabylonEngine);
-    // Creates and positions a free camera
-    const camera = new BABYLON.FreeCamera("camera1",
-        new BABYLON.Vector3(0, 5, -10), scene);
-    // Targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
-    // Creates a light, aiming 0,1,0 - to the sky
-    const light = new BABYLON.HemisphericLight("light",
-        new BABYLON.Vector3(0, 1, 0), scene);
-    // Dim the light a small amount - 0 to 1
-    light.intensity = 0.7;
-    // Built-in 'sphere' shape.
-    sphere = BABYLON.MeshBuilder.CreateSphere("sphere",
-        { diameter: 2, segments: 32 }, scene);
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-    // Built-in 'ground' shape.
-    const ground = BABYLON.MeshBuilder.CreateGround("ground",
-        { width: 10, height: 10 }, scene);
-    // Load the carpet texture
-    const carpetMat = new BABYLON.StandardMaterial("carpetMat");
-    carpetMat.diffuseTexture = new BABYLON.Texture(Assets.textures.carpet.Url, scene);
-    ground.material = carpetMat;
 
-    scene.registerBeforeRender(function () {
-        if (!scene.isReady()) {
-            BabylonEngine.stopRenderLoop();
-        }
-    });
+    //createGround(scene);
 
-    scene.executeWhenReady(function () {
-        document.getElementById("spinner").classList.toggle("hidden");
-        BabylonEngine.runRenderLoop(function () {
-            move_player();
-            scene.render();
-        })
-    });
+    var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, BABYLON.Vector3.Zero(), scene);
+    camera.attachControl(canvas, true, false);
+
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+
+    //  SKYBOX
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 5000.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture(Assets.textures.sky.Url, scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.disableLighting = true;
+    skybox.material = skyboxMaterial;
+
+    // Water
+    var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 2048, 2048, 16, scene, false);
+    var water = new BABYLON.WaterMaterial("water", scene, new BABYLON.Vector2(512, 512));
+    water.backFaceCulling = true;
+    water.bumpTexture = new BABYLON.Texture(Assets.textures.water.Url, scene);
+    water.windForce = -10;
+    water.waveHeight = 1.7;
+    water.bumpHeight = 0.1;
+    water.windDirection = new BABYLON.Vector2(1, 1);
+    water.waterColor = new BABYLON.Color3(0, 0, 221 / 255);
+    water.colorBlendFactor = 0.0;
+    water.addToRenderList(skybox);
+    waterMesh.material = water;
+
+    var player = BABYLON.SceneLoader.ImportMesh("", Assets.models.player.Url, "player.glb", scene, () => {
+
+        scene.getMeshByName("__root__").position.y += 2; // Move forward along the z-axis
+        BabylonEngine.hideLoadingUI();
+    }
+    );
+
 
     return scene;
 };
-const scene = createScene(); //Call the createScene function
-// Register a render loop to repeatedly render the scene
 
+
+window.initFunction = async function () {
+
+    var asyncEngineCreation = async function () {
+        try {
+            return createDefaultEngine();
+        } catch (e) {
+            console.log("the available createEngine function failed. Creating the default engine instead");
+            return createDefaultEngine();
+        }
+    }
+
+    window.BabylonEngine = await asyncEngineCreation();
+    if (!BabylonEngine) throw 'engine should not be null.';
+    startRenderLoop(BabylonEngine, canvas);
+    window.scene = createScene();
+};
+initFunction().then(() => {
+    sceneToRender = scene
+});
 
 // Watch for browser/canvas resize events
 window.addEventListener("resize", function () {
@@ -90,20 +149,20 @@ function configure_movement_listeners() {
 function move_player() {
     if (keyStatus[87]) {
         // 'W' key or up arrow key
-        sphere.position.z += movementAmount; // Move forward along the z-axis
+        scene.getMeshByName("__root__").position.z += movementAmount; // Move forward along the z-axis
     }
     if (keyStatus[65]) {
         // 'A' key or left arrow key
-        sphere.position.x -= movementAmount; // Move left along the x-axis
+        scene.getMeshByName("__root__").position.x -= movementAmount; // Move left along the x-axis
     }
 
     if (keyStatus[83]) {
         // 'S' key or down arrow key
-        sphere.position.z -= movementAmount; // Move backward along the z-axis
+        scene.getMeshByName("__root__").position.z -= movementAmount; // Move backward along the z-axis
     }
 
     if (keyStatus[68]) {
         // 'D' key or right arrow key
-        sphere.position.x += movementAmount; // Move right along the x-axis
+        scene.getMeshByName("__root__").position.x += movementAmount; // Move right along the x-axis
     }
 }
